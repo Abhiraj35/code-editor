1214294d2b5735a65e9091ccdaff4cc28a1f2771
Abhiraj35 <royabhi2406@gmail.com>
2026-02-20 16:05:15 +0530

    added stater file template files and added some functionality for palyground.

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,10 +1,21 @@
 "use client"
 
 import { useParams } from 'next/navigation';
-import React from 'react'
+import { useEffect } from 'react';
+import { usePlayground } from '@/modules/playground/hooks/usePlayground';
 
 const MainPlaygroundPage = () => {
     const { id } = useParams<{id: string}>();
+
+    const { playgroundData, templateData, isLoading, error, saveTemplateData } = usePlayground(id);
+
+    useEffect(() => {
+        console.log("Playground Data:", playgroundData);
+        console.log("Template Data:", templateData);
+        console.log("Loading:", isLoading);
+        console.log("Error:", error);
+    }, [playgroundData, templateData, isLoading, error]);
+
   return (
     <div>param: {id}</div>
   )
---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+    REACT: "/vibecode-staters/react-ts",
+    NEXTJS: "/vibecode-staters/nextjs",
+    EXPRESS: "/vibecode-staters/express-simple",
+    VUE: "/vibecode-staters/vue",
+    HONO: "/vibecode-staters/hono-nodejs-starter",
+    ANGULAR: "/vibecode-staters/angular",
+}
